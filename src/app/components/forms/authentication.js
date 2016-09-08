946eeb8e420c008285000f5fdc64e088b293673a
Artem Zakharchenko <artem.zakharchenko@actum.cz>
2016-09-08 11:15:42 +0200

    jQuery and Sitecore.

--- src/app/components/forms/authentication.js
+++ src/app/components/forms/authentication.js
@@ -2,91 +2,94 @@ import Alert from '../../components/alert';
 import trim from '../../helpers/trim';
 
 export default function Auth(form) {
-    /* Inputs */
-    const birth = form.querySelector('input[name="birth"]');
-    const additional = form.querySelector('input[name="additional"]');
-    const submitBtn = form.querySelector('button[type="submit"]');
+    (function ($) {
 
-    /* Status form */
-    const status = $(form.querySelector('.status'));
+        /* Inputs */
+        const birth = form.querySelector('input[name="birth"]');
+        const additional = form.querySelector('input[name="additional"]');
+        const submitBtn = form.querySelector('button[type="submit"]');
 
-    /* Shallow validation: Check only for the empty fields */
-    function validate() {
-        const required = Array.from(form.querySelectorAll('[required]'));
-        let result = true;
-        let invalid = [];
+        /* Status form */
+        const status = $(form.querySelector('.status'));
 
-        /* Validate the inputs */
-        required.map((input) => {
-            input.classList.remove('invalid');
+        /* Shallow validation: Check only for the empty fields */
+        function validate() {
+            const required = Array.from(form.querySelectorAll('[required]'));
+            let result = true;
+            let invalid = [];
 
-            if (!input.value.trim()) {
-                input.classList.add('invalid');
-                invalid.push(input);
-                result = false;
-            }
-        });
+            /* Validate the inputs */
+            required.map((input) => {
+                input.classList.remove('invalid');
 
-        return result;
-    }
+                if (!input.value.trim()) {
+                    input.classList.add('invalid');
+                    invalid.push(input);
+                    result = false;
+                }
+            });
 
-    /* Print invalid date error */
-    function invalidMessage() {
-        return Alert('Ověření Vašich údajů se nezdařilo. Zadejte prosím správné údaje. V případě potíží se prosím obraťte na naši zákaznickou podporu.', 'error', status);
-    }
-
-    /* Handle submit */
-    submitBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        /* Empty status box */
-        $(status).empty();
-
-        /* Validate the form for missed fields */
-        const validated = validate();
-
-        if (validated) {
-            /* Validate customer's birth date */
-            const validBirth = (trim(birth.value) == trim(dob));
-
-            if (validBirth) {
-                console.warn('SEND REQUEST');
-
-                /* Validation request */
-                $.ajax({
-                    type: '',
-                    dataType: 'jsonp',
-                    url: 'http://google.com',
-                    data: {
-                        additional: additional.value
-                    },
-                    
-                    /* When success, redirect to the Offer page */
-                    success: (response) => {
-                        if (response.success) {
-                            console.warn('REDIRECT TO: Offer');
-                        } else {
-                            /* Display validation error message */
-                            invalidMessage();
-                        }
-                        console.log(response);
-                    },
-
-                    /* ERROR: No response from the server */
-                    error: (e) => {
-                        /* Redirect to System error page */
-                    }
-                });
-
-            } else {
-                /* ERROR: Invalid data */
-                invalidMessage();
-            }
-        } else {
-            /* ERROR: Missing required fields */
-            Alert('Prosím , vyplňte všechna požadovaná pole.', 'error', status);
+            return result;
         }
 
-    });
+        /* Print invalid date error */
+        function invalidMessage() {
+            return Alert('Ověření Vašich údajů se nezdařilo. Zadejte prosím správné údaje. V případě potíží se prosím obraťte na naši zákaznickou podporu.', 'error', status);
+        }
 
+        /* Handle submit */
+        submitBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            /* Empty status box */
+            $(status).empty();
+
+            /* Validate the form for missed fields */
+            const validated = validate();
+
+            if (validated) {
+                /* Validate customer's birth date */
+                const validBirth = (trim(birth.value) == trim(dob));
+
+                if (validBirth) {
+                    console.warn('SEND REQUEST');
+
+                    /* Validation request */
+                    $.ajax({
+                        type: '',
+                        dataType: 'jsonp',
+                        url: 'http://google.com',
+                        data: {
+                            additional: additional.value
+                        },
+                        
+                        /* When success, redirect to the Offer page */
+                        success: (response) => {
+                            if (response.success) {
+                                console.warn('REDIRECT TO: Offer');
+                            } else {
+                                /* Display validation error message */
+                                invalidMessage();
+                            }
+                            console.log(response);
+                        },
+
+                        /* ERROR: No response from the server */
+                        error: (e) => {
+                            /* Redirect to System error page */
+                        }
+                    });
+
+                } else {
+                    /* ERROR: Invalid data */
+                    invalidMessage();
+                }
+            } else {
+                /* ERROR: Missing required fields */
+                Alert('Prosím , vyplňte všechna požadovaná pole.', 'error', status);
+            }
+
+        });
+
+    })(jQuery);
 }
