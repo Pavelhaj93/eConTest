d16b01efe23125ea553a1fc8dd5a6dbe3bcf3006
Actum Devel <tomas@NOTEBOOK>
2016-10-10 17:57:22 +0200

    Cookie law settings

--- eContracting.Website/Areas/eContracting/Controllers/eContractingController.cs
+++ eContracting.Website/Areas/eContracting/Controllers/eContractingController.cs
@@ -1,8 +1,25 @@
-﻿using Sitecore.Mvc.Controllers;
+﻿using System.Web.Mvc;
+using eContracting.Kernel;
+using eContracting.Kernel.GlassItems.Settings;
+using Glass.Mapper.Sc;
+using Sitecore.Mvc.Controllers;
 
 namespace eContracting.Website.Areas.eContracting.Controllers
 {
     public class eContractingController : SitecoreController
     {
+        public ActionResult CookieLaw()
+        {
+            using (var sitecoreContext = new SitecoreContext())
+            {
+                var cookieLawSettings = sitecoreContext.GetItem<CookieLawSettings>(ItemIds.CookieLawSettings);
+                if (cookieLawSettings != null)
+                {
+                    return View("/Areas/eContracting/Views/CookieLaw.cshtml", cookieLawSettings);
+                }
+            }
+
+            return new EmptyResult();
+        }
     }
 }
