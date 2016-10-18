373ffb7650a2ff1fcc1ac45a1a343eeb68bb6541
Actum Devel <tomas@NOTEBOOK>
2016-10-18 17:44:19 +0200

    Document panel.

---
+++ eContracting.Kernel/Models/DocumentPanelModel.cs
@@ -0,0 +1,9 @@
+﻿namespace eContracting.Kernel.Models
+{
+    public class DocumentPanelModel
+    {
+        public string ClientId { get; set; }
+
+        public bool IsAccepted { get; set; }
+    }
+}
--- eContracting.Website/Areas/eContracting/Controllers/eContractingController.cs
+++ eContracting.Website/Areas/eContracting/Controllers/eContractingController.cs
@@ -2,6 +2,10 @@
 using System.Web.Mvc;
 using eContracting.Kernel;
 using eContracting.Kernel.GlassItems.Settings;
+using eContracting.Kernel.Helpers;
+using eContracting.Kernel.Models;
+using eContracting.Kernel.Services;
+using eContracting.Kernel.Utils;
 using Glass.Mapper.Sc;
 using Sitecore.Mvc.Controllers;
 using Log = Sitecore.Diagnostics.Log;
@@ -28,6 +32,50 @@ namespace eContracting.Website.Areas.eContracting.Controllers
             catch (Exception ex)
             {
                 Log.Error("Error when displaying cookie law", ex, this);
+                return View("/Areas/eContracting/Views/SystemError.cshtml");
+            }
+        }
+
+        public ActionResult DocumentPanel()
+        {
+            try
+            {
+                var model = new DocumentPanelModel();
+
+                var authenticationDataSessionStorage = new AuthenticationDataSessionStorage();
+                var data = authenticationDataSessionStorage.GetData();
+                model.ClientId = data.Identifier;
+
+                return View("/Areas/eContracting/Views/DocumentPanel.cshtml", model);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error when displaying document panel.", ex, this);
+                return View("/Areas/eContracting/Views/SystemError.cshtml");
+            }
+        }
+
+        public ActionResult Accept()
+        {
+            try
+            {
+                var authenticationDataSessionStorage = new AuthenticationDataSessionStorage();
+                var data = authenticationDataSessionStorage.GetData();
+
+                RweClient client = new RweClient();
+                var offerSent = client.AcceptOffer(data.Identifier);
+
+                if (!offerSent)
+                {
+                    return View("/Areas/eContracting/Views/SystemError.cshtml");
+                }
+
+                var redirectUrl = ConfigHelpers.GetPageLink(PageLinkType.ThankYou).Url;
+                return Redirect(redirectUrl);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error when accepting offer.", ex, this);
                 return View("/Areas/eContracting/Views/SystemError.cshtml");
             }
         }
