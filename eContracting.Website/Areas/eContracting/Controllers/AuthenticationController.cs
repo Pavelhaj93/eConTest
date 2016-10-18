26fd11e9f44ea3228411f1ce7f6414f850369d22
Actum Devel <tomas@NOTEBOOK>
2016-10-18 13:58:23 +0200

    Authentication rendering.

---
+++ eContracting.Website/Areas/eContracting/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using eContracting.Kernel.GlassItems.Pages;
+using eContracting.Kernel.Models;
+using Glass.Mapper.Sc.Web.Mvc;
+
+namespace eContracting.Website.Areas.eContracting.Controllers
+{
+    public class AuthenticationController : GlassController<EContractingAuthenticationTemplate>
+    {
+        [HttpGet]
+        public ActionResult Authentication()
+        {
+            return View("/Areas/eContracting/Views/Authentication.cshtml");
+        }
+
+        [HttpPost]
+        public ActionResult Authentication(AuthenticationModel model)
+        {
+            return View("/Areas/eContracting/Views/Error404.cshtml");
+        }
+    }
+}
