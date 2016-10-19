a5340d4ac42efb6eacfe731c04f460574dbfcf31
Actum Devel <tomas@NOTEBOOK>
2016-10-19 12:09:52 +0200

    Accepted offer.

---
+++ eContracting.Website/Areas/eContracting/Controllers/AcceptedOfferController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.Mvc;
+using eContracting.Kernel.GlassItems.Pages;
+using eContracting.Kernel.Helpers;
+using eContracting.Kernel.Utils;
+using Glass.Mapper.Sc.Web.Mvc;
+using Sitecore.Diagnostics;
+
+namespace eContracting.Website.Areas.eContracting.Controllers
+{
+    public class AcceptedOfferController : GlassController<EContractingAcceptedOfferTemplate>
+    {
+        [HttpGet]
+        public ActionResult AcceptedOffer()
+        {
+            try
+            {
+                RweUtils utils = new RweUtils();
+                if (!utils.IsUserInSession())
+                {
+                    return Redirect(ConfigHelpers.GetPageLink(PageLinkType.SessionExpired).Url);
+                }
+
+                var authenticationDataSessionStorage = new AuthenticationDataSessionStorage();
+                var data = authenticationDataSessionStorage.GetData();
+
+                ViewData["MainText"] = Context.MainText.Replace("{0}", data.LastName);
+
+                return View("/Areas/eContracting/Views/AcceptedOffer.cshtml");
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error when displaying accepted offer.", ex, this);
+                return View("/Areas/eContracting/Views/SystemError.cshtml");
+            }
+        }
+    }
+}
