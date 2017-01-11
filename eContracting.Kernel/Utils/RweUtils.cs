0a908bde99910d4da82358d7ed6b329f4a1bac60
Masoud Azizmohammadi <masoud.mohammadi@actum.cz>
2017-01-11 12:43:26 +0100

    Home page added

---
+++ eContracting.Kernel/Utils/RweUtils.cs
@@ -0,0 +1,22 @@
+﻿namespace eContracting.Kernel.Utils
+{
+    public class RweUtils
+    {
+        public AuthenticationDataSessionStorage authenticationDataSessionStorage { get; set; }
+
+        public bool IsUserInSession()
+        {
+            if (authenticationDataSessionStorage == null)
+            {
+                authenticationDataSessionStorage = new AuthenticationDataSessionStorage();
+            }
+
+            if (authenticationDataSessionStorage.IsDataActive())
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
