3448b2dd0bc249c011b604e102ff9164f0710bc5
Masoud Azizmohammadi <masoud.mohammadi@actum.cz>
2017-02-15 09:12:58 +0100

    Queue for Failed accepted user added

---
+++ eContracting.Kernel/Agents/AcceptOfferJob.cs
@@ -0,0 +1,37 @@
+﻿using eContracting.Kernel.Models;
+using eContracting.Kernel.Services;
+using MongoDB.Bson;
+using MongoDB.Driver.Builders;
+using Sitecore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eContracting.Kernel.Agents
+{
+    [UsedImplicitly]
+    public class AcceptOfferJob
+    {
+        [UsedImplicitly]
+        public void Run()
+        {
+            int failtime = 0;
+            RweClient client = new RweClient();
+            var offersNotsent = client.AcceptedOfferCollection.FindAs<AcceptedOffer>(Query.And(Query.EQ("SentToService", (BsonValue)false)));
+            foreach (AcceptedOffer offer in offersNotsent.ToList())
+            {
+
+                if (client.AcceptOffer(offer.Guid))
+                {
+                    failtime++;
+                }
+                if (failtime > 3)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ eContracting.Kernel/Models/AcceptedOffer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eContracting.Kernel.Models
+{
+    public class AcceptedOffer
+    {
+        public long _id{get;set;}
+        /// <summary>
+        /// Customer Guid
+        /// </summary>
+        public string Guid { get; set; }
+
+        /// <summary>
+        /// If true The accepted offer has been sent to service
+        /// </summary>
+        public bool  SentToService { get; set; }
+    }
+}
+
