56dc44af76c4546ffbe2c7bf39747626b2b524ba
milan <milan@DESKTOP-0EV1GNK>
2017-05-26 11:12:20 +0200

    Commetns added

--- eContracting.Kernel/Agents/AcceptOfferJob.cs
+++ eContracting.Kernel/Agents/AcceptOfferJob.cs
@@ -1,19 +1,29 @@
-﻿using eContracting.Kernel.Models;
-using eContracting.Kernel.Services;
-using MongoDB.Bson;
-using MongoDB.Driver.Builders;
-using Sitecore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="AcceptOfferJob.cs" company="Actum">
+// Copyright © 2016 Respective owners
+// </copyright>
 
 namespace eContracting.Kernel.Agents
 {
+    using eContracting.Kernel.Models;
+    using eContracting.Kernel.Services;
+    using MongoDB.Bson;
+    using MongoDB.Driver.Builders;
+    using Sitecore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Implementation of the accepted offer job.
+    /// </summary>
     [UsedImplicitly]
     public class AcceptOfferJob
     {
+        /// <summary>
+        /// RUn method for the job.
+        /// </summary>
         [UsedImplicitly]
         public void Run()
         {
--- eContracting.Kernel/Exceptions/DateOfBirthWrongFormatException.cs
+++ eContracting.Kernel/Exceptions/DateOfBirthWrongFormatException.cs
@@ -1,18 +1,37 @@
-﻿using System;
+﻿// <copyright file="AcceptOfferJob.cs" company="Actum">
+// Copyright © 2016 Respective owners
+// </copyright>
 
 namespace eContracting.Kernel.Exceptions
 {
+    using System;
+
+    /// <summary>
+    /// Implementation of wrong birth date exception.
+    /// </summary>
     public class DateOfBirthWrongFormatException : Exception
     {
+        /// <summary>
+        /// Creates new instance of the <see cref="DateOfBirthWrongFormatException"/>.
+        /// </summary>
         public DateOfBirthWrongFormatException()
         {
         }
 
+        /// <summary>
+        /// Creates new instance of the <see cref="DateOfBirthWrongFormatException"/>.
+        /// </summary>
+        /// <param name="message">Exception message.</param>
         public DateOfBirthWrongFormatException(string message)
             : base(message)
         {
         }
 
+        /// <summary>
+        /// Creates new instance of the <see cref="DateOfBirthWrongFormatException"/>.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="inner"></param>
         public DateOfBirthWrongFormatException(string message, Exception inner)
             : base(message, inner)
         {
