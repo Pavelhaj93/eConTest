aeed6961af576b9413955d655aad17d0b3706a14
milan <milan@DESKTOP-0EV1GNK>
2017-05-26 11:20:41 +0200

    Comments added.

--- eContracting.Kernel/Extensions/IEnumerableExtensions.cs
+++ eContracting.Kernel/Extensions/IEnumerableExtensions.cs
@@ -1,14 +1,33 @@
-﻿using System.Linq;
+﻿// <copyright file="AcceptOfferJob.cs" company="Actum">
+// Copyright © 2016 Respective owners
+// </copyright>
 
 namespace System.Collections.Generic
 {
+    using System.Linq;
+
+    /// <summary>
+    /// Implementation of <see cref="IEnumerable"/> extension methods.
+    /// </summary>
     public static class IEnumerableExtensions
     {
+        /// <summary>
+        /// Checks itf the collection is empty or null. 
+        /// </summary>
+        /// <typeparam name="T">Type of item.</typeparam>
+        /// <param name="en">colelction to check</param>
+        /// <returns></returns>
         public static bool IsNullOrEmpty<T>(this IEnumerable<T> en)
         {
             return en == null || !en.Any();
         }
 
+        /// <summary>
+        /// Checks if the coillection is not empty.
+        /// </summary>
+        /// <typeparam name="T">Type of the item.</typeparam>
+        /// <param name="en">Enumarable to check.</param>
+        /// <returns></returns>
         public static bool IsNotNullOrEmpty<T>(this IEnumerable<T> en)
         {
             return !en.IsNullOrEmpty<T>();
--- eContracting.Kernel/Extensions/ZCCH_CACHE_GETResponseExtensions.cs
+++ eContracting.Kernel/Extensions/ZCCH_CACHE_GETResponseExtensions.cs
@@ -1,7 +1,15 @@
 ﻿using System.Collections.Generic;
 
+/// <summary>
+/// Extensions for SAP proxy.
+/// </summary>
 public static class ZCCH_CACHE_GETResponseExtensions
 {
+    /// <summary>
+    /// Checks if the response contains files.
+    /// </summary>
+    /// <param name="res">Response from SAP.</param>
+    /// <returns>Retruns tru if response contains files.</returns>
     public static bool ThereAreFiles(this ZCCH_CACHE_GETResponse res)
     {
         return res != null && res.ET_FILES.IsNotNullOrEmpty();
