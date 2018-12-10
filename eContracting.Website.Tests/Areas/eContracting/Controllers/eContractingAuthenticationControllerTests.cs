60f496843d009d712656452c9786c055206db29c
Lukáš Dvořák <lukas.dvorak@actum.cz>
2018-12-10 17:31:41 +0100

    Add test projects

---
+++ eContracting.Website.Tests/Areas/eContracting/Controllers/eContractingAuthenticationControllerTests.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eContracting.Website.Tests.Areas.eContracting.Controllers
+{
+    public class eContractingAuthenticationControllerTests
+    {
+    }
+}
