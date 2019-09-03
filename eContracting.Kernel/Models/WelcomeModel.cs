3b4c2ff48db6b704ad2083d3fb19afce2e1e3be4
Tomáš Knaifl <tomas@knaifl.cz>
2019-09-03 14:58:07 +0200

    Model.

---
+++ eContracting.Kernel/Models/WelcomeModel.cs
@@ -0,0 +1,10 @@
+// <copyright file="AuthenticationModel.cs" company="Actum">
+// Copyright © 2016 Respective owners
+// </copyright>
+
+namespace eContracting.Kernel.Models
+{
+    public class WelcomeModel
+    {
+    }
+}
