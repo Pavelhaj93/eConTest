882554d70a4d4efc6c75f5ff0408ee09e4e3c7d7
Tomáš Knaifl <tomas@knaifl.cz>
2019-09-17 14:30:54 +0200

    Styling fixes.

--- eContracting.Kernel/App_Start/GlassMapperSc.cs
+++ eContracting.Kernel/App_Start/GlassMapperSc.cs
@@ -8,7 +8,6 @@ DO NOT CHANGE THIS FILE - UPDATE GlassMapperScCustom.cs
 using Glass.Mapper.Maps;
 using Glass.Mapper.Sc.Configuration.Fluent;
 using Glass.Mapper.Sc.IoC;
-using Glass.Mapper.Sc.Pipelines.GetChromeData;
 using Sitecore.Pipelines;
 
 // WebActivator has been removed. If you wish to continue using WebActivator uncomment the line below
@@ -17,29 +16,30 @@ using Sitecore.Pipelines;
 
 namespace eContracting.Kernel.App_Start
 {
-	public class  GlassMapperSc
-	{
-		public void Process(PipelineArgs args){
-			GlassMapperSc.Start();
-		}
+    public class GlassMapperSc
+    {
+        public void Process(PipelineArgs args)
+        {
+            GlassMapperSc.Start();
+        }
 
-		public static void Start()
-		{
-			//install the custom services
-			var resolver = GlassMapperScCustom.CreateResolver();
+        public static void Start()
+        {
+            //install the custom services
+            var resolver = GlassMapperScCustom.CreateResolver();
 
-			//create a context
-			var context = Glass.Mapper.Context.Create(resolver);
+            //create a context
+            var context = Glass.Mapper.Context.Create(resolver);
 
-			LoadConfigurationMaps(resolver, context);
+            LoadConfigurationMaps(resolver, context);
 
-			context.Load(      
-				GlassMapperScCustom.GlassLoaders()        				
-				);
+            context.Load(
+                GlassMapperScCustom.GlassLoaders()
+                );
 
-			GlassMapperScCustom.PostLoad();
+            GlassMapperScCustom.PostLoad();
 
-			//EditFrameBuilder.EditFrameItemPrefix = "Glass-";
+            //EditFrameBuilder.EditFrameItemPrefix = "Glass-";
 
         }
 
@@ -60,6 +60,6 @@ namespace eContracting.Kernel.App_Start
             SitecoreFluentConfigurationLoader configurationLoader = configurationMap.GetConfigurationLoader<SitecoreFluentConfigurationLoader>();
             context.Load(configurationLoader);
         }
-	}
+    }
 }
 #endregion
