903c4932f526f2e7dd86cf1e76555ffe33504986
Tomáš Knaifl <tomas@knaifl.cz>
2019-10-24 12:31:03 +0200

    Fixes of the default values of labels.

--- eContracting.Kernel/Helpers/AuthenticationHelper.cs
+++ eContracting.Kernel/Helpers/AuthenticationHelper.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using eContracting.Kernel.GlassItems.Settings;
 using eContracting.Kernel.Models;
 using eContracting.Kernel.Services;
 using eContracting.Kernel.Utils;
@@ -21,7 +17,7 @@ namespace eContracting.Kernel.Helpers
         {
             this.authSession = authSessionStorage;
 
-            if(offer == null) throw new NullReferenceException("Offer can not be null");
+            if (offer == null) throw new NullReferenceException("Offer can not be null");
 
             if (offer.OfferInternal.Body.OfferIsRetention)
             {
@@ -68,8 +64,8 @@ namespace eContracting.Kernel.Helpers
 
         public string GetRealAdditionalValue(string key)
         {
-            var tmp =  this.authProcessor.GetRealAdditionalValue(key);
-            return tmp == null ? tmp : tmp.Trim().Replace(" ", string.Empty).ToLower();
+            var tmp = this.authProcessor.GetRealAdditionalValue(key);
+            return tmp == null ? tmp : tmp.Trim().Replace(" ", string.Empty).ToLower().GetHashCode().ToString();
         }
     }
 }
