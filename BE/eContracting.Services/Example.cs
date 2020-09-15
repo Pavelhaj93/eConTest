e0d9804920401eeb9ff837c52341b2e4767af03d
Lukáš Dvořák <LukasDvorak@users.noreply.github.com>
2020-09-15 17:03:27 +0200

    Add "connected service" as ZCCH_CACHE_API

---
+++ BE/eContracting.Services/Example.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eContracting.Services
+{
+    class Example
+    {
+        public SAP.ZCCH_CACHE_API Api { get; set; }
+
+        public async Task ABC()
+        {
+            var result = await Api.ZCCH_CACHE_GETAsync(null);
+        }
+    }
+}
