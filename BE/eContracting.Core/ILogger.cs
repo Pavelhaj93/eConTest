aa1b41c6b30bdf717e03fc0306ea5be28df6d475
Lukáš Dvořák <LukasDvorak@users.noreply.github.com>
2020-11-09 11:50:21 +0100

    Add documentation for ILogger

--- BE/eContracting.Core/ILogger.cs
+++ BE/eContracting.Core/ILogger.cs
@@ -11,22 +11,69 @@ namespace eContracting
     /// </summary>
     public interface ILogger
     {
+        /// <summary>
+        /// Add <c>info</c> log <paramref name="message"/>.
+        /// </summary>
+        /// <param name="guid">The unique identifier.</param>
+        /// <param name="message">The message.</param>
         void Info(string guid, string message);
 
+        /// <summary>
+        /// Add <c>debug</c> log <paramref name="message"/>.
+        /// </summary>
+        /// <param name="guid">The unique identifier.</param>
+        /// <param name="message">The message.</param>
         void Debug(string guid, string message);
 
+        /// <summary>
+        /// Add <c>warning</c> log <paramref name="message"/>.
+        /// </summary>
+        /// <param name="guid">The unique identifier.</param>
+        /// <param name="message">The message.</param>
         void Warn(string guid, string message);
 
+        /// <summary>
+        /// Add <c>error</c> log <paramref name="message"/>.
+        /// </summary>
+        /// <param name="guid">The unique identifier.</param>
+        /// <param name="message">The message.</param>
         void Error(string guid, string message);
 
+        /// <summary>
+        /// Add <c>error</c> log message from <paramref name="exception"/> with its details.
+        /// </summary>
+        /// <param name="guid">The unique identifier.</param>
+        /// <param name="exception">The exception.</param>
         void Error(string guid, Exception exception);
 
+        /// <summary>
+        /// Add <c>error</c> log <paramref name="message"/> with <paramref name="exception"/> details.
+        /// </summary>
+        /// <param name="guid">The unique identifier.</param>
+        /// <param name="message">The message.</param>
+        /// <param name="exception">The exception.</param>
         void Error(string guid, string message, Exception exception);
 
+        /// <summary>
+        /// Add <c>fatal</c> log <paramref name="message"/>.
+        /// </summary>
+        /// <param name="guid">The unique identifier.</param>
+        /// <param name="message">The message.</param>
         void Fatal(string guid, string message);
 
+        /// <summary>
+        /// Add <c>fatal</c> log message from <paramref name="exception"/> with its details.
+        /// </summary>
+        /// <param name="guid">The unique identifier.</param>
+        /// <param name="exception">The exception.</param>
         void Fatal(string guid, Exception exception);
 
+        /// <summary>
+        /// Add <c>fatal</c> log <paramref name="message"/> with <paramref name="exception"/> details.
+        /// </summary>
+        /// <param name="guid">The unique identifier.</param>
+        /// <param name="message">The message.</param>
+        /// <param name="exception">The exception.</param>
         void Fatal(string guid, string message, Exception exception);
     }
 }
--- BE/eContracting.Core/SitecoreLogger.cs
+++ BE/eContracting.Core/SitecoreLogger.cs
@@ -15,46 +15,55 @@ namespace eContracting
     [ExcludeFromCodeCoverage]
     public class SitecoreLogger : ILogger
     {
+        /// <inheritdoc/>
         public void Info(string guid, string message)
         {
             Log.Info($"[{guid}] {message}", this);
         }
 
+        /// <inheritdoc/>
         public void Debug(string guid, string message)
         {
             Log.Info($"[{guid}] {message}", this);
         }
 
+        /// <inheritdoc/>
         public void Warn(string guid, string message)
         {
             Log.Warn($"[{guid}] {message}", this);
         }
 
+        /// <inheritdoc/>
         public void Error(string guid, string message)
         {
             Log.Error($"[{guid}] {message}", this);
         }
 
+        /// <inheritdoc/>
         public void Error(string guid, Exception exception)
         {
             Log.Error($"[{guid}] {exception.Message}", exception, this);
         }
 
+        /// <inheritdoc/>
         public void Error(string guid, string message, Exception exception)
         {
             Log.Error($"[{guid}] {message}", exception, this);
         }
 
+        /// <inheritdoc/>
         public void Fatal(string guid, string message)
         {
             Log.Fatal($"[{guid}] {message}", this);
         }
 
+        /// <inheritdoc/>
         public void Fatal(string guid, Exception exception)
         {
             Log.Fatal($"[{guid}] {exception.Message}", this);
         }
 
+        /// <inheritdoc/>
         public void Fatal(string guid, string message, Exception exception)
         {
             Log.Fatal($"[{guid}] {message}", exception, this);
