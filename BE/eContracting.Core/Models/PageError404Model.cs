14cc4445372fd723cc780726234fb94de799ba01
Lukáš Dvořák <LukasDvorak@users.noreply.github.com>
2021-01-12 12:46:30 +0100

    Fix system error view

--- BE/eContracting.Core/Models/PageError404Model.cs
+++ BE/eContracting.Core/Models/PageError404Model.cs
@@ -11,16 +11,10 @@ namespace eContracting.Models
     /// <summary>
     /// Represents page model for error page 404.
     /// </summary>
-    /// <seealso cref="eContracting.Models.BaseSitecoreModel" />
+    /// <seealso cref="BasePageModel" />
     [SitecoreType(TemplateId = "{6CCFEF10-7A13-47F8-AAF3-237085B9C556}", AutoMap = true)]
-    public class PageError404Model : BaseSitecoreModel
+    public class PageError404Model : BasePageModel
     {
-        /// <summary>
-        /// Gets or sets the page title.
-        /// </summary>
-        [SitecoreField]
-        public virtual string PageTitle { get; set; }
-
         /// <summary>
         /// Gets or sets the page title.
         /// </summary>
