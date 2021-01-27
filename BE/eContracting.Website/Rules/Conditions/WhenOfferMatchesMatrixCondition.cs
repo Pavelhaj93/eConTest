e8c87d5ab490c441664b06f1a17ba6ee789472dd
Lukáš Dvořák <LukasDvorak@users.noreply.github.com>
2021-01-27 18:27:03 +0100

    Add WhenOfferMatchesMatrixCondition

---
+++ BE/eContracting.Website/Rules/Conditions/WhenOfferMatchesMatrixCondition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using Sitecore.Rules;
+using Sitecore.Rules.Conditions;
+
+namespace eContracting.Website.Rules.Conditions
+{
+    public class WhenOfferMatchesMatrixCondition<T> : WhenCondition<T> where T : RuleContext
+    {
+        public string MatrixId { get; set; }
+
+        public WhenOfferMatchesMatrixCondition()
+        {
+        }
+
+        protected override bool Execute(T ruleContext)
+        {
+            return false;
+        }
+    }
+}
