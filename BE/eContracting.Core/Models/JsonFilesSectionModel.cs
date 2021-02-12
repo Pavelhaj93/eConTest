8099b3a479c82aece29b4e9626e764efba3f7bd5
Lukáš Dvořák <LukasDvorak@users.noreply.github.com>
2021-02-12 10:48:33 +0100

    TPLES-181 | Order sections in accepted offer

--- BE/eContracting.Core/Models/JsonFilesSectionModel.cs
+++ BE/eContracting.Core/Models/JsonFilesSectionModel.cs
@@ -14,15 +14,38 @@ namespace eContracting.Models
 
         [JsonProperty("files")]
         public IEnumerable<JsonFileModel> Files { get; set; }
+        
+        [JsonIgnore]
+        public readonly int Position;
 
-        public JsonFilesSectionModel()
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonFilesSectionModel"/> class.
+        /// </summary>
+        protected JsonFilesSectionModel()
         {
         }
 
-        public JsonFilesSectionModel(IEnumerable<JsonFileModel> files, string title)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonFilesSectionModel"/> class.
+        /// </summary>
+        /// <param name="files">The files.</param>
+        /// <param name="title">The title.</param>
+        public JsonFilesSectionModel(IEnumerable<JsonFileModel> files, string title) : this(files, title, 100)
+        {
+            
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonFilesSectionModel"/> class.
+        /// </summary>
+        /// <param name="files">The files.</param>
+        /// <param name="title">The title.</param>
+        /// <param name="position">The position.</param>
+        public JsonFilesSectionModel(IEnumerable<JsonFileModel> files, string title, int position)
         {
             this.Files = files;
             this.Title = title;
+            this.Position = position;
         }
     }
 }
