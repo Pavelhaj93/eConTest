931a13dc8192d206d847ecfe9434343a35f851be
Lukáš Dvořák <lukas.dv@email.cz>
2021-11-22 16:53:35 +0100

    Add suppliers list selection in offer

---
+++ BE/eContracting.Website/Areas/eContracting2/Models/ListItemViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using eContracting.Models;
+using Newtonsoft.Json;
+
+namespace eContracting.Website.Areas.eContracting2.Models
+{
+    public class ListItemViewModel
+    {
+        [JsonProperty("label")]
+        public string Label { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
+
+        public ListItemViewModel()
+        {
+        }
+
+        public ListItemViewModel(ListItemModel model)
+        {
+            this.Label = model.Text;
+            this.Value = model.Value;
+        }
+    }
+}
---
+++ BE/eContracting.Website/Areas/eContracting2/Models/ListViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using eContracting.Models;
+using Newtonsoft.Json;
+
+namespace eContracting.Website.Areas.eContracting2.Models
+{
+    public class ListViewModel
+    {
+        [JsonProperty("label")]
+        public string Label { get; set; }
+
+        [JsonProperty("items")]
+        public IEnumerable<ListItemViewModel> Items { get; set; }
+
+        public ListViewModel()
+        {
+        }
+
+        public ListViewModel(ListCollectionModel model)
+        {
+            this.Items = model.Items.Select(x => new ListItemViewModel(x));
+        }
+    }
+}
