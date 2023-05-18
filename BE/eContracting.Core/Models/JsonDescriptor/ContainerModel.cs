8f0000dbc354855d177eddf5773f902e8f50b8a7
Lukáš Dvořák <ldvorak@actum.cz>
2023-05-18 14:34:04 +0200

    Add "data" to ContainerModel
    Related Work Items: #1167

--- BE/eContracting.Core/Models/JsonDescriptor/ContainerModel.cs
+++ BE/eContracting.Core/Models/JsonDescriptor/ContainerModel.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace eContracting.Models.JsonDescriptor
 {
     public class ContainerModel
     {
+        [JsonProperty("data")]
         public List<IDataModel> Data { get; } = new List<IDataModel>();
     }
 }
